1a817ca95e05a267aac3ca3d005ac07ead6a1be8
Rahi776 <rahirahi9900@gmail.com>
2021-11-16 08:45:05 +0500

    this is final update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) =>{
-  res.send("Hello this is NodeJS Project Done by Rehmat Ali.");
+  res.send("Hello this is NodeJS Project  by Rehmat Ali.");
 })
 const port= process.env.port || 3000;
 app.listen(port, () =>{
